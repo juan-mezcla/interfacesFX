088912d891246ac8523760055e3d4c975170f715
alumno alumno@DAM206 <juanmezcua96@gmail.com>
2025-12-15 09:30:25 +0100

    avanzado en funcionalidad, quedan retoques por hacer

--- src/main/java/actividad_2/Escena_Botones.java
+++ src/main/java/actividad_2/Escena_Botones.java
@@ -1,6 +1,7 @@
 package actividad_2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -13,7 +14,14 @@ public class Escena_Botones {
     private Label contador;
     @FXML
     private Label teclaPulsada;
-
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn_Reset;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btn_Cancel;
     private int cont=1;
     private KeyCode codigoTecla;
 
@@ -22,20 +30,39 @@ public class Escena_Botones {
         teclaPulsada.setText("");
     }
 
-    public void cancel(MouseEvent mouseEvent) {
-
+    public void eventoBtns(MouseEvent mouseEvent) {
+        cont_botones((Button) mouseEvent.getTarget());
     }
 
     public void teclaPulsada(KeyEvent keyEvent) {
-        /*
-         if(){
-            codigoTecla= keyEvent.getCode();
+
+         if(teclaPulsada.getText().contains("Label") || teclaPulsada.getText().contains(keyEvent.getText())){
+             contador.setText("Se ha pulsado la tecla "+cont+" veces");
+             teclaPulsada.setText(keyEvent.getText());
+             cont++;
+
         }else{
+            cont=1;
+             contador.setText("Se ha pulsado la tecla "+cont+" veces");
+             teclaPulsada.setText(keyEvent.getText());
+        }
+
+
+
+
+    }
+
+    public void cont_botones(Button btnSeleccionado){
+        if(teclaPulsada.getText().contains(btnSeleccionado.getText())){
+            contador.setText("Se ha pulsado el boton "+btnSeleccionado.getText()+" "+cont+" veces");
+            teclaPulsada.setText(btnSeleccionado.getText());
+            cont++;
+
+        }else{
+            cont=1;
+            contador.setText("Se ha pulsado el boton "+btnSeleccionado.getText()+" "+cont+" veces");
+            teclaPulsada.setText(btnSeleccionado.getText());
 
         }
-        * */
-
-
-
     }
 }
